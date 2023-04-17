79067f1686f1e122bd7e0f18df508d6ef00dda99
Brad Larson <larson@sunsetlakesoftware.com>
2023-04-17 17:33:17 -0600

    Point to avoidingStateMachineCoW branch on postgres-nio.

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .library(name: "PostgresKit", targets: ["PostgresKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/postgres-nio.git", from: "1.12.0"),
+        .package(url: "https://github.com/BradLarson/postgres-nio.git", branch: "avoidingStateMachineCoW"),
         .package(url: "https://github.com/vapor/sql-kit.git", from: "3.22.0"),
         .package(url: "https://github.com/vapor/async-kit.git", from: "1.14.0"),
         .package(url: "https://github.com/apple/swift-atomics.git", from: "1.0.0")
